58cfc9d680ec8229ca1a49e460a46b3f52b69231
maximov <maximov-ru>
2020-05-04 19:24:12 +0300

    test

--- target.js
+++ target.js
@@ -1,1 +1,14 @@
-console.log('this is target code');
+function processReceivedCommand(evt)
+{
+    console.log('evt', evt);
+}
+
+$(document).ready(() => {
+   $('#root').text('document loaded');
+    function init()
+    {
+        var Socket = new WebSocket('ws://' + window.location.hostname + ':81/');
+        Socket.onmessage = function(event) { processReceivedCommand(event); };
+    }
+    init()
+});
